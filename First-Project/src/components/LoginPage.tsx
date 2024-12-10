230f860214d1eec974f36e3db35f22f53e33f1ac
VInnyLeo <83982184+VInnyLeo@users.noreply.github.com>
2024-12-10 18:57:13 -0500

    Login and LogOut

--- First-Project/src/components/LoginPage.tsx
+++ First-Project/src/components/LoginPage.tsx
@@ -3,10 +3,28 @@ import { useAuthContext } from "../context/AuthContext"
 export default function LoginPage() {
 
   useAuthContext();
+  const {state, loginEmailWithPassword, logOut} = useAuthContext();
+
+  console.log(state)
+
+  if (state === 1){
+    return (
+      <>
+        <h3>Tu estas logeado</h3>
+        <button onClick={()=>loginEmailWithPassword('vinny@correo.com', '12345')} className="bg-blue-500 text-white rounded-2xl mt-2 p-5">Login</button>
+        
+      </>
+    )
+  }
+
 
   return (
     <>
         <h3>Bienvenidos a la website</h3>
+
+        <h3>Ingresar</h3>
+
+        <button onClick={()=>logOut('vinny@correo.com', '12345')} className="bg-blue-500 text-white rounded-2xl mt-2 p-5">Log Out</button>
     
     </>
   )
--- First-Project/src/context/AuthContext.tsx
+++ First-Project/src/context/AuthContext.tsx
@@ -7,8 +7,13 @@ enum StateTest {
     'authenticated'
 }
 
+interface AuthState{
+    state: StateTest;
+    loginEmailWithPassword: (mail: string, password: string) => void
+}
+
 //es como una tienda, debe ser exportable para poder usarlo 
-export const AuthContext = createContext ({});
+export const AuthContext = createContext ({} as AuthState);
 
 export const useAuthContext = () => useContext(AuthContext)
 //PropsWithChildren tipo de dato q nos dice que va a ser un componenete hijo
@@ -16,12 +21,23 @@ export const AuthProvider = ({children}:PropsWithChildren) => {
 
     const [state, setstate] = useState(StateTest.checking);
 
+    const loginEmailWithPassword = (email: string, password: string) => {
+        setstate(StateTest.login)
+    }
+
+    const logOut = () => {
+        setstate(StateTest.closed)
+    }
+
     return (
         <AuthContext.Provider value={{
-            stateValue: state
+            state: state,
+            loginEmailWithPassword,
+            logOut
         }}>
             {children}
         </AuthContext.Provider>
+        
     )
 
 }
